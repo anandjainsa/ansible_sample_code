a3b322d37958a1d8e0df6efea50de0c50fd5809f
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-10-29 13:32:45 +0530

    Update git_clone.sh

--- git_clone.sh
+++ git_clone.sh
@@ -2,13 +2,15 @@
 
 function clone_pull {
   DIRECTORY=$(basename "$1" .git)
-  if [ -d "$DIRECTORY" ]; then
-    cd "$DIRECTORY"
+  if [ -d "$2/$DIRECTORY" ]; then
+    cd "$2/$DIRECTORY"
     git pull
-    cd ../
+    cd -
   else
+    mkdir -p $2 && cd $2
     git clone "$1"
   fi
 }
 
-clone_pull https://github.com/anandjainsa/ansible_sample_code
+
+clone_pull https://github.com/anandjainsa/ansible_sample_code anandjain
