fb21b3849dcb4de396849c47bb2bc4f07d05e555
anandjainsa <53891867+anandjainsa@users.noreply.github.com>
2020-11-02 15:36:26 +0530

    Create git_clone_updated.sh

---
+++ git_clone_updated.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function clone_pull {
+  DIRECTORY=$(basename "$1" .git)
+  if [ -d "$2/$DIRECTORY" ]; then
+    cd "$2/$DIRECTORY"
+    echo "this $2/$DIRECTORY"
+
+    if [ -d ".git" ]; then
+      git pull origin master
+      cd -
+    else
+      git init
+      git remote add origin $1.git
+      git pull origin master
+      cd -
+    fi
+  else
+    mkdir -p $2 && cd $2
+    git clone "$1"
+    cd -
+  fi
+}
+
+#clone_pull <GIT_URL> <PATH>
+clone_pull https://github.com/anandjainsa/ansible_sample_code anandjain
